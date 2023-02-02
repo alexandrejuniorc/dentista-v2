62885886e17f048b3dfd370492becfb24b211525
Alexandre Junior <alexandrejunior408@gmail.com>
2023-02-01 21:37:10 -0300

    feat: configurações de header e footer em volta do site, e configuração de página inicial

---
+++ src/pages/_app.page.tsx
@@ -0,0 +1,16 @@
+import { Footer } from '../components/Footer'
+import { Header } from '../components/Header'
+import type { AppProps } from 'next/app'
+import { globalStyles } from '../styles/global'
+
+globalStyles()
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  )
+}
---
+++ src/pages/_document.page.tsx
@@ -0,0 +1,43 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+import { getCssText } from '../styles'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap"
+          rel="stylesheet"
+        />
+
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;900&display=swap"
+          rel="stylesheet"
+        />
+
+        <style
+          id="stitches"
+          dangerouslySetInnerHTML={{ __html: getCssText() }}
+        ></style>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
