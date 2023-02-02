ec02bc4b8f8915925a3b3bd4794c79ec9661c62c
Alexandre Junior <alexandrejunior408@gmail.com>
2023-02-01 21:35:44 -0300

    feat: configurações iniciais

---
+++ src/styles/index.ts
@@ -0,0 +1,38 @@
+import { createStitches } from '@stitches/react'
+
+export const {
+  config,
+  styled,
+  css,
+  globalCss,
+  keyframes,
+  getCssText,
+  theme,
+  createTheme,
+} = createStitches({
+  theme: {
+    colors: {
+      white: '#FFF',
+
+      gray1200: '#000000',
+      gray1100: '#111111',
+      gray1000: '#2E2E2E ',
+      gray900: '#404040',
+      gray800: '#595959',
+      gray700: '#717171',
+      gray600: '#9C9C9C',
+      gray500: '#B2B2B2',
+      gray400: '#CCCCCC',
+      gray300: '#DEDEDE',
+      gray200: '#EDEDED',
+      gray100: '#F7F7F7',
+
+      brown100: '#695958',
+      yellow100: '#FDFDDB',
+
+      blue300: '#00AFB9',
+      blue200: '#0081A7',
+      blue100: '#6D9DC5',
+    },
+  },
+})
